693c269f17114bbe02d45c678837261842075fe7
Артур <arturskomarohov@gmail.com>
2021-08-23 19:49:47 +0300

    testExchange done

---
+++ DextPractic.Tests/ExchangeTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+using BankSystem.Services;
+using BankSystem.Models;
+
+namespace DextPractic.Tests
+{
+    public class ExchangeTest
+    {
+        [Fact]
+        public void GetExchange_10_USD_Eq_163_RUB()
+        {
+            //Arrange
+            var testExchange = new Exchange();
+            //Act 
+            var result = testExchange.ConvertCurrency<Currency>(10, new USD(), new RUB());
+
+            //Assert;
+            Assert.Equal(163, result);
+
+        }
+    }
+}
